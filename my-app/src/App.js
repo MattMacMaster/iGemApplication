4dd1444c7fb74b8bb3793815c4932b9f22c89aaf
Lance Tapang <lance.tapang@uleth.ca>
2026-01-30 12:19:54 -0700

    Start up work for sidemenu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidemenu from './Components/Sidemenu';
 import { useState, useCallback } from 'react';
 import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
@@ -7,23 +8,7 @@ import '@xyflow/react/dist/style.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Lethbridge iGem
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidemenu />
     </div>
   );
 }
---
+++ my-app/src/Components/Sidemenu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Sidemenu() {
+    return (
+        <div className = "Sidemenu">
+        </div>
+    )
+}
+
+export default Sidemenu
