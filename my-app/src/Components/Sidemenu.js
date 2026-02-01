275e7cd2447cc7d1b5af84294bbc87ee14c25720
Lance Tapang <lance.tapang@uleth.ca>
2026-01-31 20:31:28 -0700

    added the hardware parts inside side menu because i keep forgetting what we using :/

--- my-app/src/Components/Sidemenu.js
+++ my-app/src/Components/Sidemenu.js
@@ -1,5 +1,14 @@
 
 const Sidemenu = ({ isOpen, toggleMenu }) => {
+    const parts = [
+        'Steppermotor',
+        'Peristaltic Pump',
+        'Spectrometer',
+        'Syringe Pump',
+        'Electroporator',
+        'Thermometer'
+    ]
+
     return (
         <div className={`Sidemenu ${isOpen ? 'open' : 'closed'}`}>
             <ul>
